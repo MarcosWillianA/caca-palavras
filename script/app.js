f422b4f480ff6fba778f5784fe02066e9f54bbf0
Marcos Willian <marcoswillianl1987@gmail.com>
2024-10-22 16:38:04 -0400

    nome completo
    consegui inserir um nome inteiro verticalmente

--- script/app.js
+++ script/app.js
@@ -1,6 +1,5 @@
 const container = document.querySelector('#container');
-const nomes = ['marcos', 'rosângela', 'alcilene', 'suzana', 'ângela'];
-const letrasDoAlfabeto = ['a', 'b', 'c', 'ç', 'd', 'e']
+const nomes = ['marcos', 'rosângela', 'alcilene', 'suzana', 'ângela', 'pedro', 'joão', 'érica', 'tomás', 'fernando', 'bruna', 'viviane', 'débora'];
 const orientacao = ['vertical', 'horizontal'];
 
 function criarGrid () {
@@ -22,9 +21,8 @@ function escolherNome () {
     return arrayDeLetras;
 }
 
-console.log(escolherNome());
 nomeEscolhido = escolherNome();
-console.log(nomeEscolhido.length)
+console.log(nomeEscolhido);
 
 function verticalHorizontal () {
     n = Math.floor(Math.random () * orientacao.length);
@@ -38,10 +36,24 @@ function escolherPosicao () {
     return n;
 }
 
-console.log(escolherPosicao());
+posicaoEscolhida = escolherPosicao();
+console.log(`Posição escolhida no grid: ${posicaoEscolhida}`);
 
-function inserirNome() {
-    celulas[escolherPosicao()].innerHTML = nomeEscolhido[0];
+function coordenadasLetras () {
+    nomeEscolhido.forEach((letra, indice) => {
+        console.log(celulas[indice * 12 + posicaoEscolhida].innerHTML = letra);
+        celulasCertas = indice * 12 + posicaoEscolhida; // Consegui as coordenadas que queria
+        //console.log(celulasCertas);
+        
+    })
+    
 }
 
-inserirNome();
+celulasCertas = coordenadasLetras();
+console.log(celulasCertas);
+/*
+    PRÓXIMOS PASSOS: 
+    - Verificar se há espaço pra inserir a palavra
+    - Caso não haja, escolher outra orientação
+    - De alguma forma, colocar cada letra no array de posições correto
+*/
